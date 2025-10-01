d7f76adb23ff04d69c988c91e263a9fdbd0cb918
unknown <jeww1234@gmail.com>
2025-10-01 20:17:51 +0900

    메인 skills-html

--- src/component/Aboutme.jsx
+++ src/component/Aboutme.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Aboutme = () => {
   return (
     <section>
-      Aboutme
+      <h2></h2>
+      <p>자기소개 어필</p>
+      <ul>
+        자신있는 기술
+        <li>
+          <img src="" alt="" />
+          <p></p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <p></p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <p></p>
+        </li>
+      </ul>
+      <ul>
+        학교? 스터디? 부캠?
+        <li>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+            <p></p>
+          </div>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+            <p></p>
+          </div>
+        </li>
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Aboutme
+export default Aboutme;
--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
     <section>
-      skills
+      <h2>My Skills</h2>
+      <p>Skills & Attributes</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vitae
+        natus possimus saepe, aliquid reiciendis atque dolores temporibus
+        quaerat enim labore, non illum sit placeat quasi aspernatur, facilis
+        obcaecati? Odit.
+      </p>
+      <div>
+        <section>
+          <h3>Coding Skills</h3>
+          <ul>
+            <li>Html Css</li>
+            <li>Bootstrap</li>
+            <li>Tailwind Css</li>
+            <li>Java Script</li>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>Python</li>            
+            <li>Vite</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Tools</h3>
+          <ul>
+            <li>Visual Studio Code</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+        <section>
+          <h3>Etc</h3>
+          <ul>
+            <li>Git</li>
+            <li>Figma</li>
+            <li>Notion</li>
+          </ul>
+        </section>
+      </div>
+      <p></p>
     </section>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
