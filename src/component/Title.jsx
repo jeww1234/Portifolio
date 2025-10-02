d62670c3efacd94966ac30562e61e52f6fccee99
unknown <jeww1234@gmail.com>
2025-10-02 20:01:36 +0900

    title-css

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Title = () => {
   return (
-    <section>
-      <img src="./images/favicon.png" alt="진혁이 아이콘" />
-      <h2>Hello I'm a tenacious frontend developer</h2>
-      <p>꿈을 꾸는 프론트앤드 개발자입니다.</p>
-      <button><a href="#">Github</a></button>
-      <button><a href="#">Velog</a></button>
+    <section id='home'>
+      <img src="./images/favicon.png" alt="진혁이 아이콘"  className='home-avatar'/>
+      <h2 className='home-title'>Hello<br></br> I'm a tenacious <strong className='home-title--strong'>frontend developer</strong></h2>
+      <p className='home-description'>꿈을 꾸는 프론트앤드 개발자입니다.</p>
+      <button><a href="#" className='home-git'>Github</a></button>
+      <button><a href="#" className='home-vel'>Velog</a></button>
     </section>
   )
 }
