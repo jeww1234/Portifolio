66deefd842fe2932ac63daa4902a3164704cbbb8
unknown <jeww1234@gmail.com>
2025-10-02 19:51:57 +0900

    Header nav

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <header style={{backgroundColor:"pink"}}>
-      <img src="./images/favicon.png" alt="" />
-      <h1><a href="#">Jinhyuk</a></h1>
+    <header className="header">
+      <div className="header-logo">
+        <img src="./images/favicon.png" alt="" className="header-logo-img" />
+        <h1 className="header-logo-title">
+          <a href="#">Jinhyuk</a>
+        </h1>
+      </div>
 
       <nav>
-        <ul>
-          <li><a href="">About me</a></li>
-          <li><a href="">Skills</a></li>
-          <li><a href="">My works</a></li>
-          <li><a href="">Community</a></li>          
+        <ul className="header-menu">
+          <li>
+            <a href="" className="header-menu-item active">
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="" className="header-menu-item">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="" className="header-menu-item">
+              My works
+            </a>
+          </li>
+          <li>
+            <a href="" className="header-menu-item">
+              Community
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
