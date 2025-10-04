2d530245c9d2433b653b14f742f3651f251cd566
unknown <jeww1234@gmail.com>
2025-10-04 18:15:30 +0900

    js-header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const [isDark, setIsDark] = useState(false);
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    if (headerRef.current) {
+      const height = headerRef.current.getBoundingClientRect().height;      
+      setHeaderHeight(height);
+    }
+
+    const handleScrool = () => {
+      const scrolly = window.scrollY;
+      setIsDark(scrolly > headerHeight);
+    };
+
+    window.addEventListener("scroll", handleScrool);
+    return () => window.removeEventListener("scroll", handleScrool);
+  }, [headerHeight]);
+
   return (
-    <header className="header">
+    <header className={`header ${isDark ? "dark" : ""}`} ref={headerRef}>
       <div className="header-logo">
         <img src="./images/favicon.png" alt="" className="header-logo-img" />
         <h1 className="header-logo-title">
